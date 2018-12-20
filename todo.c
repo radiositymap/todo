2776cd2f823bcf54af09a945870e682afed43a3e
- <c_rowley@mail.com>
2018-12-20 11:35:46 +0000

    Update todo.c

--- todo.c
+++ todo.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
 #define COL_R "\x1b[31m"
 #define COL_G "\x1b[32m"
@@ -9,20 +11,39 @@
 #define COL_C "\x1b[36m"
 #define COL_X "\x1b[00m"
 
-enum Board {
+#define FILENAME "list.txt"
+#define DELIM ","
+#define MAX_BOARDS 10
+#define MAX_ITEMS 50
+
+const char colours[][6] = {
+    COL_C,
+    COL_R,
+    COL_Y,
+    COL_G,
+    COL_M
+};
+
+enum ItemFields {
+    DESC_I,
+    DUE_I,
+    BOARD_I
+};
+
+enum Boards {
     BACKLOG,
     TODO,
     DOING,
     DONE
 };
 
-typedef struct itemStruct {
+typedef struct ItemStruct {
     char desc[60];
     char due[10];
     int board;
 } Item;
 
-void testDisplay(void) {
+void test_display(void) {
     printf(COL_R "Test" COL_X "\n");
     printf(COL_G "Test" COL_X "\n");
     printf(COL_Y "Test" COL_X "\n");
@@ -31,7 +52,7 @@ void testDisplay(void) {
     printf(COL_X "Test" COL_X "\n");
 }
 
-void displayHeader(void) { // colour codes are counted as extra chars
+void display_header(void) { // colour codes are counted as extra chars
     printf("%-30s%-30s%-30s\n", 
             COL_R "TODO", COL_Y "DOING", COL_G "DONE" COL_X);
 }
@@ -77,13 +98,96 @@ void display(Item* todo, size_t todo_i,
     int i;
     int max_i = max(todo_i, max(doing_i, done_i));
     for (i=0; i<max_i; i++) {
-        printf("%-25s%-25s%-25s\n", todo[i].desc, doing[i].desc, done[i].desc);
-        printf(COL_B "%25s%25s%25s\n" COL_X, todo[i].due, doing[i].due, done[i].due);
+        printf("%-25s%-25s%-25s\n",
+                todo[i].desc, doing[i].desc, done[i].desc);
+        printf(COL_B "%25s%25s%25s\n" COL_X, 
+                todo[i].due, doing[i].due, done[i].due);
     }
 }
 
+void write(FILE *f, char* header, Item* items, size_t size) {
+    int i;
+    fprintf(f, "%s\n", header);
+    for (i=0; i<size; i++) {
+        fprintf(f, "%s,%s\n", items[i].desc, items[i].due);
+    }
+}
 
+int tokenise(char line[], char delim[], char *tokens[MAX_BOARDS]) {
+    char *token;
+    int count = 0;
+
+    token = strtok(line, delim);
+    while (token != NULL && !isspace(token[0])) {
+        tokens[count] = token;
+        token = strtok(NULL, delim);
+        count++;
+    }
+
+    // trim \n off last token
+    char *pos;
+    if ((pos=strchr(tokens[count-1], '\n')) != NULL)
+        *pos = '\0';
+
+    return count;
+}
+
+int read(Item boards[MAX_BOARDS][MAX_ITEMS]) {
+    FILE* f = fopen(FILENAME, "r");
+
+    // get no of boards and board names
+    char line[500];
+    if (fgets(line, sizeof(line), f))
+        printf("%s", line);
+    int num_boards;
+    char *board_names[MAX_BOARDS];
+    num_boards = tokenise(line, DELIM, board_names);
+
+    // read to boards[][]
+    int board_i = 0;
+    int item_i = 0;
+    while (fgets(line, sizeof(line), f)) {
+        char *tokens[MAX_BOARDS];
+
+        int num_field;
+        num_field = tokenise(line, DELIM, tokens);
+
+        if (num_field <= 0) {
+            board_i++;
+            item_i=0;
+        }
+
+        if (num_field >= 2) {
+            strcpy(boards[board_i][item_i].desc, tokens[DESC_I]);
+            strcpy(boards[board_i][item_i++].due, tokens[DUE_I]);
+        }
+    }
+
+    fclose(f);
+
+    // print boards to test
+    int i, j;
+    for (i=0; i<num_boards; i++) {
+        printf("%d\n", i);
+        for (j=0; j<2; j++) {
+            printf("desc: %s, due: %s \n", boards[i][j].desc, boards[i][j].due);
+        }
+    }
+
+    return num_boards;
+}
+
+// add board
+// delete board
+// move item
 int main() {
+    Item boards[MAX_BOARDS][MAX_ITEMS] = {{{{0}}}};
+
+    // read file for no of boards
+    int num_boards = read(boards);
+    printf("no of boards: %d\n", num_boards);
+    
+    /*
     Item todo[10] = {{0}};
     Item doing[10] = {{0}};
     Item done[10] = {{0}};
@@ -101,6 +205,19 @@ int main() {
 
     displayHeader();
     display(todo, todo_i, doing, doing_i, done, done_i);
+
+    // write to file
+    FILE *f;
+    f = fopen("list.txt", "w");
+    if (f == NULL) {
+        printf("Can't open file\n");
+        exit(1);
+    }
+    write(f, "TODO", todo, todo_i);
+    write(f, "DOING", doing, doing_i);
+    write(f, "DONE", done, done_i);
+    fclose(f);
+    */
 
     return 0;
 }
